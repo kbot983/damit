bda468712af59f67876a93df3376e37de1e2406b
Shashank <Shashank>
2018-03-08 23:00:46 +0530

    Added php for OTP with expiry

--- Login and Signup/checkNo.php
+++ Login and Signup/checkNo.php
@@ -1,35 +1,39 @@
 <?php
 include "../connection.php";
 date_default_timezone_set('Asia/Kolkata');
-echo "HERE";
 $mobile_no = $_POST['number'];
 $otp = rand(1000,9999);
 $query = "SELECT * FROM `user` where mobile_no = '$mobile_no'";
 
-// if($result = mysqli_query($conn, $query)){
-//     $res = mysqli_num_rows($result);
-//     if($res > 0){
-//         echo "Exists";
-//     }else{
-//         if(mysqli_query($conn, "INSERT INTO `otp`(`mobile_no`, `otp`, `minutes`, `is_expired`) VALUES ('$mobile_no','$otp','" . date("i"). "','0')")){
-//             echo "Added";
-//             echo(date("i"));
-//         }else{
-//             echo mysqli_error($conn);
-//         }
-
-//     }
-// }
-
-
-
-// echo $date1 = date('i') . "<br>";
-
-// //sleep for 5 seconds
-// sleep(5);
-
-// //start again
-// echo $date2 = date('i');
-
-// echo (int)$date2 - (int)$date1;
+if($result = mysqli_query($conn, $query)){
+    $res = mysqli_num_rows($result);
+    if($res > 0){
+        echo "Exists";
+    }else if(mysqli_query($conn, "INSERT INTO `otp`(`mobile_no`, `otp`, `time`, `is_expired`) VALUES ('$mobile_no','$otp','" . date("h:i:s"). "','0')")){
+            echo $mobile_no;
+            echo "Added";
+            // SEND OTP 
+            $apiKey = urlencode('GwdEObV9VZA-qVJt6nGaqBp8qPjFiVoeHpjR3z8alF');
+    	
+            // Message details
+            $numbers = $mobile_no;
+            $sender = urlencode('TXTCL');
+            $message = rawurlencode("Your One Time Password for Dam IT is ".$otp.". This OTP is valid for 5 minutes.");
+            
+            // Prepare data for POST request
+            $data = array('apikey' => $apiKey, 'numbers' => $numbers, "sender" => $sender, "message" => $message);
+        
+            // Send the POST request with cURL
+            $ch = curl_init('https://api.textlocal.in/send/');
+            curl_setopt($ch, CURLOPT_POST, true);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            $response = curl_exec($ch);
+            curl_close($ch);
+            
+            echo $response;
+        }else{
+            echo mysqli_error($conn);
+        }
+}
 ?>
--- Login and Signup/checkOtp.php
+++ Login and Signup/checkOtp.php
@@ -1,1 +1,53 @@
-$otp = rand(1000,9999);
+<?php
+
+include "../connection.php";
+date_default_timezone_set('Asia/Kolkata');
+$mobile_no = $_POST['mobile_no'];
+$entered_otp = $_POST['otp_no'];
+
+// OTP -> TIME
+$query = "SELECT time from `otp` WHERE mobile_no = '$mobile_no' AND otp = '$entered_otp'";
+if($result = mysqli_query($conn, $query)){
+    $count = mysqli_num_rows($result);
+    if($count > 0){
+        while($res = mysqli_fetch_assoc($result)){
+            $saved_time = $res["time"];
+        }
+    // Check time here
+    $current_time = date("h:i:s");
+    echo $diff = round((strtotime("1/1/1980 $current_time")- strtotime("1/1/1980 $saved_time")) / 60);
+    echo "    ";
+    if($diff > 5){
+        echo "The OTP you entered has expired. Request for an OTP again!";
+        $delquery = "DELETE FROM `otp` WHERE mobile_no = '$mobile_no'";
+        $result = mysqli_query($conn, $delquery);
+        echo "Deleted.";
+    }else{
+        echo "Successful!";
+        $delquery = "DELETE FROM `otp` WHERE mobile_no = '$mobile_no'";
+        $result = mysqli_query($conn, $delquery);
+        echo "Deleted.";
+    }
+    }else{
+        echo "The OTP you entered is incorrect.";
+    }
+}else{
+    echo "ERROR";
+    echo mysqli_error($conn);
+}
+
+
+
+
+// $query = "SELECT time FROM `otp` where otp = '$entered_otp' AND mobile_no = '$mobile_no'";
+// $result = mysqli_query($conn, $query);
+// while($res= mysqli_fetch_assoc($result)){
+//     $time = $res["time"];
+// }
+// $now = date("h:i:s");
+// echo $diff = (strtotime("1/1/2018 $now") - strtotime("1/1/2018 $time"))/60;
+
+
+?>
+
+
